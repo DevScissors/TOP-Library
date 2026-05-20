1fbc857eea908809a08f52f79617a5dd19f229d5
DevScissors <sean.scissors@gmail.com>
2026-05-20 17:05:15 -0500

    Working add to library button and form resets on modal close

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,108 @@
 const cardsWrapper = document.querySelector(".cards-wrapper");
-const cardDiv = document.createElement("div");
-cardDiv.className = "book-card";
-const cardBookTitle = document.createElement("h2");
-cardBookTitle.className = "book-title";
-const cardBookAuthor = document.createElement("p");
-cardBookAuthor.className = "book-author";
-const cardBookPages = document.createElement("p");
+const addBookBtn = document.querySelector(".add-book-button");
+
+// form related DOM elements
+const modal = document.querySelector("#addBookModal");
+const formModal = document.querySelector(".add-to-library-form");
+const titleInput = document.querySelector(".book-title-input");
+const authorInput = document.querySelector(".book-author-input");
+const numPagesInput = document.querySelector(".book-pages-input");
+const readCheckbox = document.querySelector(".read-checkbox");
+const submitBtn = document.querySelector(".submit-btn");
 
 const myLibrary = [];
 
-function Book(title, author, numPages) {
+function Book(title, author, numPages, isRead) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
   this.title = title;
   this.author = author;
   this.numPages = numPages;
-  this.read = true;
+  this.isRead = isRead;
   this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(title, author, numPages) {
+function checkFormValues() {
+  if (
+    titleInput.value !== "" &&
+    authorInput.value != "" &&
+    numPagesInput.value != ""
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function addBookToLibrary() {
+  const titleValue = titleInput.value;
+  const authorValue = authorInput.value;
+  const numPagesValue = numPagesInput.value;
+  const isReadValue = readCheckbox.checked ? (isRead = "Yes") : (isRead = "No");
   // take params, create a book then store it in the array
-  const book = new Book(title, author, numPages);
+  const book = new Book(titleValue, authorValue, numPagesValue, isReadValue);
   myLibrary.push(book);
 }
 
-function buildCard() {}
-
 function displayBook() {
+  cardsWrapper.innerHTML = ""; // optional: clear existing cards
   myLibrary.forEach((book, index) => {
-    cardBookPages.className = "book-num-pages";
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "book-card";
+
+    const cardBookTitle = document.createElement("h2");
+    cardBookTitle.className = "book-title";
     cardBookTitle.textContent = `Title: ${book.title}`;
+
+    const cardBookAuthor = document.createElement("p");
+    cardBookAuthor.className = "book-author";
     cardBookAuthor.textContent = `Author: ${book.author}`;
+
+    const cardBookPages = document.createElement("p");
+    cardBookPages.className = "book-num-pages";
     cardBookPages.textContent = `Length: ${book.numPages} pages`;
 
-    cardDiv.appendChild(cardBookTitle);
-    cardDiv.appendChild(cardBookAuthor);
-    cardDiv.appendChild(cardBookPages);
+    const cardBookRead = document.createElement("p");
+    cardBookRead.className = "book-read";
+    cardBookRead.textContent = `Have you read the book: ${book.isRead}`;
+
+    cardDiv.append(cardBookTitle, cardBookAuthor, cardBookPages, cardBookRead);
     cardsWrapper.appendChild(cardDiv);
   });
 }
 
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", 293);
-addBookToLibrary("The Witcher", "Some Guy", 321);
-addBookToLibrary("To Kill a Mockingbird", "That other guy", 195);
-displayBook();
-console.log(myLibrary);
+addBookBtn.addEventListener("click", () => {
+  // open form modal and disable submit button
+  modal.showModal();
+  submitBtn.disabled = true;
+});
+
+// Check that each required input has a value, otherwise keep submit button disabled
+titleInput.addEventListener("input", () => {
+  submitBtn.disabled = !checkFormValues();
+});
+
+authorInput.addEventListener("input", () => {
+  submitBtn.disabled = !checkFormValues();
+});
+
+numPagesInput.addEventListener("input", () => {
+  submitBtn.disabled = !checkFormValues();
+});
+
+submitBtn.addEventListener("click", () => {
+  addBookToLibrary();
+  displayBook();
+  clearFormValues();
+  modal.close();
+  // e.preventDefault();
+});
+
+modal.addEventListener("close", () => {
+  clearFormValues();
+});
+
+function clearFormValues() {
+  formModal.reset();
+}
